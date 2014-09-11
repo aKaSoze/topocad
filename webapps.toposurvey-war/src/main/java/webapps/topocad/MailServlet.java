8d0ec51c25f47918ebdba23dc5f227c93a4389b9
sorin <sorin.nica@woowteam.com>
2014-09-11 19:45:26 +0300

    added mail capabilities

--- webapps.toposurvey-war/src/main/java/webapps/topocad/MailServlet.java
+++ webapps.toposurvey-war/src/main/java/webapps/topocad/MailServlet.java
@@ -2,7 +2,14 @@ package webapps.topocad;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,12 +23,31 @@ public class MailServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+
+		String msgBody = "Acest mail a fost trimis prin app engine, imi pare rau ca am uitat sa cobor cu cheia, m-am gandit ca mai bine imi ofer un motiv sa-ti trimit acest mail rezolvand problema trimiteri lui.";
+
 		// Set response content type
 		resp.setContentType("text/html");
-
-		// Actual logic goes here.
 		PrintWriter out = resp.getWriter();
-		out.println("<h1>" + "TopoCad" + "</h1>");
+
+		try {
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress("mrsoze.ks@gmail.com",
+					"App Engine"));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
+					"corina_geboiu@yahoo.com", "Yo coco"));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
+					"nica_petre_sorin@yahoo.com", "Mr. Soze"));
+			msg.setSubject("Scuze papusa");
+			msg.setText(msgBody);
+			Transport.send(msg);
+			out.println("<h1>Email Sent Neg</h1>");
+		} catch (MessagingException e) {
+			out.println("<h1>Email Was Not Sent Neg</h1>");
+			out.println("<h1>" + e.getMessage() + "</h1>");
+		}
 	}
 
 }
